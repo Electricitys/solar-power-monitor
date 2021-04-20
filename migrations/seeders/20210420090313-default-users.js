257ec00a529c42d99d8a6aa154bb0172e3ea086e
Imanuel Pundoko <ilomon10@gmail.com>
2021-04-20 18:57:22 +0800

    add migration script

---
+++ migrations/seeders/20210420090313-default-users.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const password = await bcrypt.hash("admin", 10);
+    await queryInterface.bulkInsert("users", [{
+      email: "admin",
+      password: password,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
