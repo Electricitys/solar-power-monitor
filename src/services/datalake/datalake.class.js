915e00defa3778d4f7faebc0fea610bdf60b7164
Imanuel Pundoko <ilomon10@gmail.com>
2021-06-21 17:55:50 +0800

    Sanitize incoming data

--- src/services/datalake/datalake.class.js
+++ src/services/datalake/datalake.class.js
@@ -1,5 +1,7 @@
 const { BadRequest } = require('@feathersjs/errors');
 const { Service } = require('feathers-sequelize');
+const moment = require('moment');
+const logger = require('../../logger');
 
 exports.Datalake = class Datalake extends Service {
   async create(data, params) {
@@ -8,6 +10,40 @@ exports.Datalake = class Datalake extends Service {
     if (typeof query === 'undefined') throw new BadRequest('`query` does not exist. (required)');
 
     if (typeof query['deviceId'] === 'undefined') throw new BadRequest('Device ID not found.');
+
+    const now = moment().startOf("minutes");
+    const reminder = 5 - (now.get('minute') % 5);
+    const round = moment(now)
+      .add(reminder, 'minutes')
+      .subtract(5, 'minutes')
+      ;
+
+    let { data: lastData } = await this.find({
+      query: {
+        $limit: 1,
+        deviceId: query.deviceId,
+        createdAt: {
+          $gte: round.toISOString()
+        },
+        $sort: { createdAt: -1 }
+      }
+    });
+
+    lastData = lastData[0];
+
+    if (lastData) {
+      let patchData = {};
+      Object.keys(data).forEach(key => {
+        try {
+          patchData[key] = (Number(data[key]) + Number(lastData[key])) / 2;
+        } catch (err) {
+          logger.error(err);
+        }
+      });
+      return this.patch(lastData.id, patchData);
+    }
+
+    data.createdAt = round.toISOString();
 
     return super.create({
       ...data,
